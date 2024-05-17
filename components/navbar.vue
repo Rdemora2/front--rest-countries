d2d8064fb76a7f3ee51d551d6c85889949162a82
Rdemora2 <robertomoraeszar@gmail.com>
2024-05-16 23:14:23 -0300

    REFACTOR/ ajustes na navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="w-full flex justify-center items-center h-24">
-      <UHorizontalNavigation :links="links" class="w-full flex justify-center items-center h-full">
-        <template #default="{ link }">
-          <span class="group-hover:text-primary relative" style="font-size: 1.5rem;">
-            {{ link.label }}
-          </span>
-        </template>
-      </UHorizontalNavigation>
+    <div class="w-full flex justify-center items-center h-24 rounded-b-lg bg-black">
+        <UHorizontalNavigation :links="links" class="w-full flex justify-center items-center h-full">
+          <template #default="{ link }">
+            <span class="group-hover:text-primary relative" style="font-size: 1.5rem;">
+              {{ link.label }}
+            </span>
+          </template>
+        </UHorizontalNavigation>
     </div>
   </template>
   
