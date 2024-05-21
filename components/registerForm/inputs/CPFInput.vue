01ce4712c4aeb220955c29a6b2d3a9c3fbff164d
Rdemora2 <robertomoraeszar@gmail.com>
2024-05-21 15:21:31 -0300

    REFACTOR/ reorganiza pastas dos componentes

---
+++ components/registerForm/inputs/CPFInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="mt-3">
+    <label for="cpf">{{ label }}</label>
+    <UInput
+      type="text"
+      id="cpf"
+      v-model="formattedCPF"
+      @input="formatCPF"
+      required
+    />
+    <p v-if="cpfInvalid" class="text-red-500 text-sm mt-1">CPF inválido</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { cpf } from "cpf-cnpj-validator";
+
+const emits = defineEmits(["cpfInvalid", "updateCpf"]);
+const label = "CPF";
+const formattedCPF = ref("");
+const cpfInvalid = ref(false);
+
+const formatCPF = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  const unformattedCPF = input.value.replace(/[^\d]/g, "");
+  const formatted = cpf.format(unformattedCPF);
+  formattedCPF.value = formatted;
+  input.value = formatted;
+  validateCPF();
+};
+
+const validateCPF = () => {
+  const isValidCPF = cpf.isValid(formattedCPF.value);
+  cpfInvalid.value = !isValidCPF;
+  emits("updateCpf", formattedCPF.value);
+  emits("cpfInvalid", cpfInvalid.value);
+  if (!isValidCPF) {
+    console.error("CPF inválido");
+    return;
+  }
+};
+</script>
---
+++ components/registerForm/inputs/moneyInput.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="mt-3">
+    <label :for="id">{{ label }}</label>
+    <UInput
+      type="text"
+      :id="id"
+      v-model="completeValue"
+      @input="onInput"
+      required
+    />
+    <p v-if="showMinValueWarning" class="text-red-500 text-sm mt-1">
+      O value mínimo é R$ 1.000,00
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const emits = defineEmits(["updateIncome", "showMinValueWarning"]);
+const id = "income";
+const inputValue = ref("");
+const label = "Renda mensal";
+const showMinValueWarning = ref(false);
+
+function checkMinimumValue(value: string): void {
+  const valueNumeric = parseFloat(value.replace(/[^\d]/g, ""));
+  if (valueNumeric < 100000) {
+    showMinValueWarning.value = true;
+  } else {
+    showMinValueWarning.value = false;
+  }
+  emits("showMinValueWarning", showMinValueWarning.value);
+}
+
+function formatCurrency(value: string): string {
+  const valueNumeric = value.replace(/[^\d]/g, "");
+  const numericFormatedValue = (parseFloat(valueNumeric) / 100)
+    .toFixed(2)
+    .replace(".", ",")
+    .replace(/(\d)(?=(\d{3})+\,)/g, "$1.");
+  const completeValue = `R$ ${numericFormatedValue}`;
+  const formatedNumber = parseFloat(valueNumeric) / 100;
+  const formFormatNumber = Math.floor(formatedNumber);
+  emits("updateIncome", formFormatNumber);
+  return completeValue;
+}
+
+function onInput(event: InputEvent): void {
+  let value = (event.target as HTMLInputElement).value;
+
+  if (!/^\d*\.?\d*$/.test(value)) {
+    (event.target as HTMLInputElement).value = value.replace(/[^\d]/g, "");
+    value = (event.target as HTMLInputElement).value;
+  }
+
+  const valueFormatted = formatCurrency(value);
+  inputValue.value = valueFormatted;
+  checkMinimumValue(value);
+}
+
+const completeValue = computed(() => inputValue.value);
+</script>
