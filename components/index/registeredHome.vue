5fb1a5680f8e851c480da5b49e5ed29622356341
Rdemora2 <robertomoraeszar@gmail.com>
2024-05-21 17:50:19 -0300

    FEAT/ cria exibição condicional da homepage de acordo com cadastro do user

---
+++ components/index/registeredHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="container mx-auto px-4 py-8 bg-gray-900 min-h-screen flex flex-wrap justify-center"
+  >
+    <UCard class="mb-12 mx-4 w-80 text-center">
+      <template #header>
+        <h2 class="text-2xl font-bold mb-4 text-white">
+          Parabéns! Você está autenticado
+        </h2>
+      </template>
+      <p class="text-lg text-gray-400 mb-4">
+        A partir de agora você pode interagir com uma aplicação web capaz de
+        obter dados de uma API pública com informações de países
+      </p>
+    </UCard>
+  </div>
+</template>
+
+<script setup></script>
