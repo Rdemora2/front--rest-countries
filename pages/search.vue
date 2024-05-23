ca67b571df8aa15169499ad4b96d16d5feb4a44a
Rdemora2 <robertomoraeszar@gmail.com>
2024-05-22 22:34:18 -0300

    STYLE/ ajustes basicos no style do chart e search

--- pages/search.vue
+++ pages/search.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto px-4 py-8 bg-gray-900 min-h-screen flex flex-wrap justify-center">
+  <div class="container mx-auto px-8 py-8 bg-gray-900 min-h-screen flex flex-wrap justify-center">
         search
     </div>
 </template>
@@ -8,7 +8,5 @@
 .container {
   max-width: 100%;
   min-height: calc(100vh - 7.3rem);
-  padding-left: 2rem;
-  padding-right: 2rem;
 }
 </style>
