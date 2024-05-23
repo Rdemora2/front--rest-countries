ba069cf56089ae4fc68a1fd1776bb5d64432198a
Rdemora2 <robertomoraeszar@gmail.com>
2024-05-22 22:33:34 -0300

    FEAT/ create userData

---
+++ pages/userData.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container mx-auto px-8 py-8 bg-gray-900 min-h-screen flex flex-wrap justify-center">
+        userData
+      </div>
+  </template>
+  
+  <style scoped>
+  .container {
+    max-width: 100%;
+    min-height: calc(100vh - 7.3rem);
+  }
+  </style>
+  
+  <script>
+</script>
