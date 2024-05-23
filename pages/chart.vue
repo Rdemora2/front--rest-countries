d0056c53def26fff6a6f0efe92119dee121720a6
Rdemora2 <robertomoraeszar@gmail.com>
2024-05-23 13:51:10 -0300

    FEAT/ cria versão inicial da tela do grafico

--- pages/chart.vue
+++ pages/chart.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="container mx-auto px-8 py-8 bg-gray-900 min-h-screen flex flex-wrap justify-center">
-        chart
-    </div>
+  <div
+    class="container mx-auto px-8 py-8 bg-gray-900 min-h-screen flex flex-wrap justify-center"
+  >
+    <q-page>
+      <l-map v-if="!loading" :markers="markers" :geojsonData="geojsonData" />
+    </q-page>
+  </div>
 </template>
 
 <style scoped>
@@ -10,3 +14,31 @@
   min-height: calc(100vh - 7.3rem);
 }
 </style>
+
+<script>
+import { defineComponent, ref, onMounted } from "vue";
+import LMap from "~/components/geoChart.vue";
+import africaData from "~/public/data/geoJson/africa.geo.json";
+
+export default defineComponent({
+  name: "MapPage",
+  components: {
+    LMap,
+  },
+  setup() {
+    const markers = ref([]);
+    const loading = ref(true);
+    const geojsonData = ref(africaData);
+
+    onMounted(() => {
+      loading.value = false;
+    });
+
+    return {
+      markers,
+      loading,
+      geojsonData,
+    };
+  },
+});
+</script>
