21425df0987cfa8b16a2ce58d186100070912eba
Rdemora2 <robertomoraeszar@gmail.com>
2024-05-23 14:32:28 -0300

    FEAT/ versão inicial do mapa demografico

--- components/geoChart.vue
+++ components/geoChart.vue
@@ -8,6 +8,7 @@
 import { defineComponent, onMounted, onBeforeUnmount } from "vue";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import axios from "axios";
 
 export default defineComponent({
   name: "LMap",
@@ -22,8 +23,9 @@ export default defineComponent({
     let map = null;
     let geojsonLayer = null;
 
-    onMounted(() => {
-      createMapLayer();
+    onMounted(async () => {
+      const countriesData = await fetchCountriesData();
+      createMapLayer(countriesData);
     });
 
     onBeforeUnmount(() => {
@@ -32,22 +34,109 @@ export default defineComponent({
       }
     });
 
-    const createMapLayer = () => {
+    const fetchCountriesData = async () => {
+      try {
+        const response = await axios.get(
+          "https://restcountries.com/v3.1/all?fields=name,population"
+        );
+        return response.data;
+      } catch (error) {
+        console.error("Erro ao buscar dados da API:", error);
+        return [];
+      }
+    };
+
+    const createMapLayer = (countriesData) => {
       map = L.map("mapContainer").setView([-26.8560346, -49.239189], 5);
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      if (props.geojson) {
-        setGeoJSON();
+      if (props.geojson && countriesData.length > 0) {
+        const countriesGeojson = filterCountriesGeojson(props.geojson, countriesData);
+        setGeoJSON(countriesGeojson);
       }
     };
 
-    const setGeoJSON = () => {
-      geojsonLayer = L.geoJSON(props.geojson).addTo(map);
+    const filterCountriesGeojson = (geojson, countriesData) => {
+      const countryPopulationMap = {};
+      countriesData.forEach(country => {
+        countryPopulationMap[country.name.common] = country.population;
+      });
+
+      geojson.features.forEach(feature => {
+        const countryName = feature.properties.name;
+        if (countryPopulationMap.hasOwnProperty(countryName)) {
+          feature.properties.population = countryPopulationMap[countryName];
+        }
+      });
+
+      return geojson;
+    };
+
+    const setGeoJSON = (countriesGeojson) => {
+      geojsonLayer = L.geoJSON(countriesGeojson, {
+        style: style,
+        onEachFeature: onEachFeature
+      }).addTo(map);
+
       map.fitBounds(geojsonLayer.getBounds());
     };
+
+    // Obtém a cor dependendo da população
+    function getColor(population) {
+      return population > 100000000 ? '#800026' :
+             population > 50000000  ? '#BD0026' :
+             population > 20000000  ? '#E31A1C' :
+             population > 10000000  ? '#FC4E2A' :
+             population > 5000000   ? '#FD8D3C' :
+             population > 2000000   ? '#FEB24C' :
+             population > 1000000   ? '#FED976' : '#FFEDA0';
+    }
+
+    // Estilo do país no mapa
+    function style(feature) {
+      return {
+        fillColor: getColor(feature.properties.population),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+      };
+    }
+
+    // Adiciona eventos aos países
+    function onEachFeature(feature, layer) {
+      layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+      });
+    }
+
+    // Destaque do país ao passar o mouse
+    function highlightFeature(e) {
+      const layer = e.target;
+
+      layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+      });
+
+      layer.bringToFront();
+    }
+
+    function resetHighlight(e) {
+      geojsonLayer.resetStyle(e.target);
+    }
+
+    function zoomToFeature(e) {
+      map.fitBounds(e.target.getBounds());
+    }
   },
 });
 </script>
